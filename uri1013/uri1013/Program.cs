b2ecad6b87e589a084c11766ba782e923ccf70b0
Adriel Pailos <adrielpailos@gmail.com>
2022-08-20 17:30:15 -0300

    1013cs

---
+++ uri1013/uri1013/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a, b, c, maiorAb, maior;
+
+        string[] val = Console.ReadLine().Split(' ');
+        a = int.Parse(val[0]);
+        b = int.Parse(val[1]);
+        c = int.Parse(val[2]);
+
+        maiorAb = (a + b + Math.Abs(a - b)) / 2;
+        maior = (maiorAb + c + Math.Abs(maiorAb - c)) / 2;
+
+        Console.WriteLine(maior + " eh o maior");
+    }
+}
