620b2f8a69392090d2e3ecd28795aac81172ddf7
Adriel Pailos <adrielpailos@gmail.com>
2022-08-20 17:24:02 -0300

    1008cs

---
+++ uri1008/uri1008/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int funcionario, horasTrabalhadas;
+        double valorPorHora, salario;
+
+        funcionario = int.Parse(Console.ReadLine());
+        horasTrabalhadas = int.Parse(Console.ReadLine());
+        valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        salario = horasTrabalhadas * valorPorHora;
+
+        Console.WriteLine("NUMBER = " + funcionario);
+        Console.WriteLine("SALARY = U$ " + salario.ToString("f2", CultureInfo.InvariantCulture));
+    }
+}
