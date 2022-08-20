e35a957ca15acd19091127cdc0d6587545814e3b
Adriel Pailos <adrielpailos@gmail.com>
2022-08-20 17:27:20 -0300

    1011cs

---
+++ uri1011/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        int raio;
+        double pi, volume;
+
+        raio = int.Parse(Console.ReadLine());
+        pi = 3.14159;
+        volume = 4.0 / 3.0 * pi * raio * raio * raio;
+        Console.WriteLine("VOLUME = " + volume.ToString("f3", CultureInfo.InvariantCulture));
+    }
+}
+
