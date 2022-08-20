c8c8b0640d4aec78a3b600dfb3fe9d7b26c664ea
Adriel Pailos <adrielpailos@gmail.com>
2022-08-20 17:03:44 -0300

    1002cs

---
+++ uri1002/uri1002/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double pi, area, raio;
+
+        pi = 3.14159;
+        raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        area = pi * raio * raio;
+
+        Console.WriteLine("A=" + area.ToString("f4", CultureInfo.InvariantCulture));
+
+        Console.ReadLine();
+    }
+}
