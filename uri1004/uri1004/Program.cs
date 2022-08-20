80ad42bfdc4b7e363d4b8322e9b0177ac7cad8e6
Adriel Pailos <adrielpailos@gmail.com>
2022-08-20 17:18:25 -0300

    1004cs

---
+++ uri1004/uri1004/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int A, B, prod;
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+
+        prod = A * B;
+        Console.WriteLine("PROD = " + prod);
+    }
+}
