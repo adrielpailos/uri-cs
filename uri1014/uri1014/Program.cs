84522e14a106b2a8489644842e702b0ab457b74a
Adriel Pailos <adrielpailos@gmail.com>
2022-08-20 17:31:14 -0300

    1014cs

---
+++ uri1014/uri1014/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x;
+        double y, kmPorLitro;
+
+        x = int.Parse(Console.ReadLine());
+        y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        kmPorLitro = x / y;
+
+        Console.WriteLine(kmPorLitro.ToString("f3", CultureInfo.InvariantCulture) + " km/l");
+    }
+}
