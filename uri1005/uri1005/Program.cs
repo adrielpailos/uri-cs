8ab6142d4e0ea3956ce18d472543617ce80662f2
Adriel Pailos <adrielpailos@gmail.com>
2022-08-20 17:19:26 -0300

    1005cs

---
+++ uri1005/uri1005/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double A, B, media;
+        A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        media = ((A * 3.5) + (B * 7.5)) / 11;
+        Console.WriteLine("MEDIA = " + media.ToString("f5", CultureInfo.InvariantCulture));
+        Console.ReadLine();
+    }
+}
