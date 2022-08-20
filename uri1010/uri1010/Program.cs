85f19d606976fe733c6e52e92c52ebd82b0d71ba
Adriel Pailos <adrielpailos@gmail.com>
2022-08-20 17:26:30 -0300

    1010cs

---
+++ uri1010/uri1010/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int codigo1, nDePecas1, codigo2, nDePecas2 ;
+        double valorUnitario1, valorUnitario2, valorFinal;
+
+        string[] total = Console.ReadLine().Split(' ');
+        codigo1 = int.Parse(total[0]);
+        nDePecas1 = int.Parse(total[1]);
+        valorUnitario1 = double.Parse(total[2], CultureInfo.InvariantCulture);
+
+        total = Console.ReadLine().Split(' ');
+        codigo2 = int.Parse(total[0]);
+        nDePecas2 = int.Parse(total[1]);
+        valorUnitario2 = double.Parse(total[2], CultureInfo.InvariantCulture);
+        valorFinal = nDePecas2 * valorUnitario1 + nDePecas2 * valorUnitario2;
+
+        Console.WriteLine("VALOR A PAGAR: R$ " + valorFinal.ToString("f2", CultureInfo.InvariantCulture));
+    }
+}
