750f427d3cca6cf97af1496a8b8e949c7b3fb39f
Adriel Pailos <adrielpailos@gmail.com>
2022-08-20 17:25:00 -0300

    1009cs

---
+++ uri1009/uri1009/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+class UProgram
+{
+    static void Main(string[] args)
+    {
+        string nome;
+        double salarioFixo, totalDeVendas, comicaoDeVendas;
+
+        nome = Console.ReadLine();
+        salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        totalDeVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        comicaoDeVendas = totalDeVendas * 15 / 100 + salarioFixo;
+
+        Console.WriteLine("TOTAL = R$ " + comicaoDeVendas.ToString("f2", CultureInfo.InvariantCulture));
+    }
+}
