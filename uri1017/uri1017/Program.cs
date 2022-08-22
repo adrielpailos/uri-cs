ad5d24ae75b01c3c42ea00b44af9a59e6f376c63
Adriel Pailos <adrielpailos@gmail.com>
2022-08-22 17:51:27 -0300

    1017cs

---
+++ uri1017/uri1017/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempoGasto, velocidadeMedia, distancia;
+            double litros;
+
+            tempoGasto = int.Parse(Console.ReadLine());
+            velocidadeMedia = int.Parse(Console.ReadLine());
+
+            distancia = tempoGasto * velocidadeMedia;
+            litros = distancia / 12.0;
+
+            Console.WriteLine(litros.ToString("f3", CultureInfo.InvariantCulture));
+        }
+    }
+}
