72a7196ff0469f5b436685ca52f4985181f5fe51
Adriel Pailos <adrielpailos@gmail.com>
2022-09-28 16:15:02 -0300

    uriCS

---
+++ uri1018/uri1018/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, menorNuDnotas, nota, resto;
+
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n);
+
+            resto = n;
+
+            nota = 100;
+            menorNuDnotas = resto / nota;
+            Console.WriteLine(menorNuDnotas + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 50;
+            menorNuDnotas = resto / nota;
+            Console.WriteLine(menorNuDnotas + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 20;
+            menorNuDnotas = resto / nota;
+            Console.WriteLine(menorNuDnotas + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 10;
+            menorNuDnotas = resto / nota;
+            Console.WriteLine(menorNuDnotas + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 5;
+            menorNuDnotas = resto / nota;
+            Console.WriteLine(menorNuDnotas + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            nota = 2;
+            menorNuDnotas = resto / nota;
+            Console.WriteLine(menorNuDnotas + " nota(s) de R$ " + nota + ",00");
+            resto = resto % nota;
+
+            Console.WriteLine(resto + " nota(s) de R$ 1,00");
+        }
+    }
+}
